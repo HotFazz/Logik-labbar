ba4ccd62d7a51d9ebcbe1fd6ec9147539e0671e9
HotFazz <basalts.darter-0a@icloud.com>
2023-10-02 21:07:24 +0200

    Lonely commit

---
+++ assignment1/unifering.pl
@@ -0,0 +1,29 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% uppgift 1	(4p)
+% unifiering
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Betrakta denna fråga till ett Prologsystem:
+%
+% ?- T=f(a,Y,Z), T=f(X,X,b).
+%
+% Vilka bindningar presenteras som resultat?
+%
+% Ge en kortfattad förklaring till ditt svar!
+
+
+% The given query is 
+
+?- T=f(a,Y,Z), T=f(X,X,b).
+
+% If we break down this query 
+
+% T is unified with f(a, Y, Z), and is therfore bound to it
+% T is also bound to f(X,X,b), meaning that f(a,Y,Z) is unified with f(X,X,b) as they have the same functor and same number of aritry
+% This in turn results in 
+    % a being unified with X
+    % Y being unified with X
+    % Z being unified with b
+
+
---
+++ defenitioner.pl
@@ -0,0 +1,31 @@
+% predikat som du kan använda:
+% Andra predikat som definierats i bibliotek i ert 
+% prologsystem får inte användas. Skriv definitionen
+% explicit, kanske med ett annat namn,
+% så att den inte krockar.
+
+append([],L,L).
+append([H|T],L,[H|R]) :- append(T,L,R).
+
+appendEl(X, [], [X]).
+appendEl(X, [H | T], [H | Y]) :-
+           appendEl(X, T, Y).
+
+length([],0).
+length([_|T],N) :- length(T,N1), N is N1+1.
+
+nth(N,L,E) :- nth(1,N,L,E).
+nth(N,N,[H|_],H).
+nth(K,N,[_|T],H) :- K1 is K+1, nth(K1,N,T,H).
+
+subset([], []).
+subset([H|T], [H|R]) :- subset(T, R).
+subset([_|T], R) :- subset(T, R).
+
+select(X,[X|T],T).
+select(X,[Y|T],[Y|R]) :- select(X,T,R).
+
+member(X,L) :- select(X,L,_).
+
+memberchk(X,L) :- select(X,L,_), !.
+
