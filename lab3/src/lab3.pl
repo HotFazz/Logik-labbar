acc4621f1884d5cb44fc0cb85fdedee5178544db
HotFazz <basalts.darter-0a@icloud.com>
2023-12-13 15:33:07 +0100

    commenting and other stuff

--- lab3/src/lab3.pl
+++ lab3/src/lab3.pl
@@ -1,105 +1,121 @@
 % Load model, initial state and formula from file.
 verify(Input) :-
   see(Input), read(T), read(L), read(S), read(F), seen,
-  validate(T, L, S, [], F).
+  check(T, L, S, [], F).
+
 
 % and
-validate(Transitions, Labels, State, [], and(F1, F2)) :-
-  validate(Transitions, Labels, State, [], F1),
-  validate(Transitions, Labels, State, [], F2).
+check(Transitions, Labels, State, [], and(F1, F2)) :-
+  check(Transitions, Labels, State, [], F1),
+  check(Transitions, Labels, State, [], F2).
 
 % or
-validate(Transitions, Labels, State, [], or(F, _)) :-
-  validate(Transitions, Labels, State, [], F), !.
-validate(Transitions, Labels, State, [], or(_, F)) :-
-  validate(Transitions, Labels, State, [], F), !.
+check(Transitions, Labels, State, [], or(F, _)) :-
+  check(Transitions, Labels, State, [], F), !.
+
+check(Transitions, Labels, State, [], or(_, F)) :-
+  check(Transitions, Labels, State, [], F), !.
+
 
 % ax
-validate(Transitions, Labels, State, U, ax(F)) :-
+% all next
+% For all paths the next step satisfies F
+check(Transitions, Labels, State, U, ax(F)) :-
   getList(Transitions, State, Paths),
-  validate_next(Transitions, Labels, Paths, U, F).
+  check_next(Transitions, Labels, Paths, U, F).
+
 
 % ex
-validate(Transitions, Labels, State, U, ex(F)) :-
-  % Get all available paths from the given State.
-  getList(Transitions, State, States),
+% exists next
+% There exists a path where the next state satisfies F
+check(Transitions, Labels, State, U, ex(F)) :-
+  % Get all available paths from the given State
 
+  getList(Transitions, State, States),
   member(S1, States),
-  validate(Transitions, Labels, S1, U, F).
+  check(Transitions, Labels, S1, U, F).
+
 
 % ag
-validate(_, _, State, U, ag(_)) :-
-
+% always globally 
+% For all paths, in all states, F is satisfied
+check(_, _, State, U, ag(_)) :-
   member(State, U).
-validate(Transitions, Labels, State, U, ag(F)) :-
-  
-  validate(Transitions, Labels, State, [], F),
+check(Transitions, Labels, State, U, ag(F)) :-
+  check(Transitions, Labels, State, [], F),
+  check(Transitions, Labels, State, [State|U], ax(ag(F))).
 
-  validate(Transitions, Labels, State, [State|U], ax(ag(F))).
 
 % eg
-validate(_, _, State, U, eg(_)) :-
-  % Stop if the current state has been visited before.
-
+% exists globally
+% There exists a path where in all states, F is satisfied
+check(_, _, State, U, eg(_)) :-   %Stop if visited
   member(State, U), !.
 
-validate(Transitions, Labels, State, U, eg(F)) :-
-  % Ensure the formula holds in the current state.
-
-  validate(Transitions, Labels, State, [], F),
-  % Get all paths from the current state.
-
+check(Transitions, Labels, State, U, eg(F)) :-    % Check the formula holds in the current state
+  check(Transitions, Labels, State, [], F),       % Get all paths from the current state
   getList(Transitions, State, Paths),
-  % See ex/5 for further comments.
-
   member(S1, Paths),
-  validate(Transitions, Labels, S1, [State|U], eg(F)).
+  check(Transitions, Labels, S1, [State|U], eg(F)).
+
 
 % ef
-validate(Transitions, Labels, State, U, ef(F)) :-   % Ensure the current state has not been visited before.
+% exists finally
+% There exists a path where F will eventually be satisfied
+check(Transitions, Labels, State, U, ef(F)) :-   % Check the current state has not been visited 
+  \+ member(State, U),                           % Check the formula holds in the current state
+  check(Transitions, Labels, State, [], F).
 
-  \+ member(State, U),    % Ensure the formula holds in the current state.
 
-  validate(Transitions, Labels, State, [], F).
+check(Transitions, Labels, State, U, ef(F)) :-   % Check the current state has not been visited before
 
-validate(Transitions, Labels, State, U, ef(F)) :-
-  % Ensure the current state has not been visited before.
-
-  \+ member(State, U),    % Get all paths from the current state.
-  getList(Transitions, State, Paths),   % See ex/5 for furter comments.
+  \+ member(State, U),                           % Get all paths from the current state
+  getList(Transitions, State, Paths),   
   member(S1, Paths),
-  validate(Transitions, Labels, S1, [State|U], ef(F)).
+  check(Transitions, Labels, S1, [State|U], ef(F)).
+
+
 
 % af
-validate(Transitions, Labels, State, U, af(F)) :-   % Ensure the current state has not been visited before.
-  \+ member(State, U),    % Ensure the formula holds in the current state.
-  validate(Transitions, Labels, State, [], F).
+% or all future
+% For all paths, F will eventually be satisfied
+check(Transitions, Labels, State, U, af(F)) :-   % Check that the current state has not been visited 
+  \+ member(State, U),                           % Check that the formula holds in the current 
+  check(Transitions, Labels, State, [], F).      
 
 
-validate(Transitions, Labels, State, U, af(F)) :-
-  % See ef/5 for furher comments.
+check(Transitions, Labels, State, U, af(F)) :-
   \+ member(State, U),
-  validate(Transitions, Labels, State, [State|U], ax(af(F))).
+  check(Transitions, Labels, State, [State|U], ax(af(F))).
  
+
 % neg
-validate(_, Labels, State, [], neg(F)) :-
-  getList(Labels, State, Formulas),
-  % Ensure the given formula is not present in the list of formulas that holds
+% The formula F is not satisfied in the current state
+check(_, Labels, State, [], neg(F)) :-
+  getList(Labels, State, Formulas),              % Check iven formula is not present in the list of formulas that holds
   % in the current state.
   \+ member(F, Formulas).
 
+
 % arbitrary formula
-validate(_, Labels, State, [], F) :-
-  % Get all formulas that holds in the current state.
+check(_, Labels, State, [], F) :-                % Get all formulas that holds in the current state
   getList(Labels, State, Formulas),
   member(F, Formulas).
 
-% Iterate over all given states and ensure the given formula holds in all states.
-validate_next(_, _, [], _, _).
-validate_next(T, L, [State|States], U, F) :-
-  validate(T, L, State, U, F), !,
-  validate_next(T, L, States, U, F).
+
+% Iterate over all given states and check the given formula holds in all 
+check_next(_, _, [], _, _).
+check_next(T, L, [State|States], U, F) :-
+  check(T, L, State, U, F), !,
+  check_next(T, L, States, U, F).
 
 
+% Helper predicates
+% retrieve a list of paths or formulas associated with a particular state
 getList([[State, Paths]|_], State, Paths) :- !.
-getList([_|T], State, Paths) :- getList(T, State, Paths).
+getList([_|T], State, Paths) :- 
+getList(T, State, Paths).
+
+
+% ! is used to prevent backtracking in certain parts of the code to ensure that once a condition is met, Prolog does not continue to search for other solutions.
+% This is used in the eg and ef functions to prevent the program from continuing to search for other solutions once a solution has been found.
